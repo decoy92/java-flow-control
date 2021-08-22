250fd12f7baca78f904cbfb11c1229acb7ad3840
decoy92 <83627908+decoy92@users.noreply.github.com>
2021-08-23 04:56:00 +0900

    Add files via upload

---
+++ java-flow-control/ComparisonOperatorApp.java
@@ -0,0 +1,14 @@
+
+public class ComparisonOperatorApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println(1 > 1); // false
+		System.out.println(1 == 2); // false
+		System.out.println(1 < 1); // false
+		System.out.println(1 >= 1); // true
+		// 비교연산자 : 왼쪽과 오른쪽의 값을 비교하여 결과에 따라 참/거짓의 값을 만들어 내는 연산자 
+
+	}
+
+}
