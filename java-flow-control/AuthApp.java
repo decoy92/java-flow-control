063f1f22490a72b8d7c7de5e1c020fc6e98397c0
decoy92 <83627908+decoy92@users.noreply.github.com>
2021-08-24 05:42:24 +0900

    Add files via upload

--- java-flow-control/AuthApp.java
+++ java-flow-control/AuthApp.java
@@ -2,8 +2,20 @@
 public class AuthApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println(args[0]); // Jin 출력 (Arguments 값으로 Jin 넣었기 때
+		
+		String id = "Jin";
+		String inputId = args[0];
+		
+		System.out.println("Hi.");
+		
+		//if(inputId == id) {
+		if(inputId.equals(id)) {
+			System.out.println("Master!");
+		} else {
+			System.out.println("Who are you?");
+		}
 	}
 
 }
