f689bfa6d3bd525252612979dce179810bf55b32
decoy92 <83627908+decoy92@users.noreply.github.com>
2021-08-25 15:53:52 +0900

    Add files via upload

--- java-flow-control/AuthApp.java
+++ java-flow-control/AuthApp.java
@@ -3,19 +3,34 @@ public class AuthApp {
 
 	public static void main(String[] args) {
 
-		System.out.println(args[0]); // Jin 출력 (Arguments 값으로 Jin 넣었기 때
+//		System.out.println(args[0]); // Jin 출력 (Arguments 값으로 Jin 넣었기 때문.
 		
 		String id = "Jin";
 		String inputId = args[0];
 		
+		String pass = "1234";
+		String inputPass = args[1]; // [1] : 두 번째 입력값을 받을 수 있다.
+		
 		System.out.println("Hi.");
 		
 		//if(inputId == id) {
-		if(inputId.equals(id)) {
-			System.out.println("Master!");
+//		if(inputId.equals(id)) {
+//			if(inputPass.equals(pass)) {
+//				System.out.println("Master!");
+//			} else {
+//				System.out.println("Wrong password!");
+//			}
+//		} else {
+//			System.out.println("Who are you?");
+//		}
+		
+		if(inputId.equals(id) && inputPass.equals(pass)) {
+				System.out.println("Master!");
 		} else {
 			System.out.println("Who are you?");
 		}
+		
+		
 	}
 
 }
